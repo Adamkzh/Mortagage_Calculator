c54a74e8226d26cf5ba919a9f8185983efd69c98
Adam Ke <kzh88@hotmail.com>
2018-03-25 23:20:34 -0700

    add Google Map

--- app/src/main/java/com/example/adam/test/MapFragment.java
+++ app/src/main/java/com/example/adam/test/MapFragment.java
@@ -2,18 +2,26 @@ package com.example.adam.test;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements OnMapReadyCallback {
 
-
+    GoogleMap map;
     public MapFragment() {
         // Required empty public constructor
     }
@@ -22,8 +30,24 @@ public class MapFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_map, container, false);
+
+        View v = inflater.inflate(R.layout.fragment_map, container, false);
+        return v;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        SupportMapFragment mapFragment =(SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng SJSU = new LatLng(37.334018, -121.8815528);
+        map.addMarker(new MarkerOptions().position(SJSU).title("San Jose State University"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(SJSU));
+    }
 }
